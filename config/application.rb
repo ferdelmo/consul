f3c7e3b1b8143227a75383ba3629bbb52232461e
ferdelmo <ferdelmo1996@gmail.com>
2018-02-21 13:42:33 +0100

    Corrigiendo configuracion de idiomas

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Consul
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
     config.i18n.available_locales = [:es]
-    config.i18n.fallbacks = {'fr' => 'es', 'pt-br' => 'es', 'nl' => 'en'}
+    config.i18n.fallbacks = {'fr' => 'es', 'pt-br' => 'es'}
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'custom', '**', '*.{rb,yml}')]
 
